89a6b4c64427ce8d5f88c78df892a4f57b59b569
FOKI89 <damien.lery@sfr.fr>
2016-05-24 09:06:02 +0200

    Progressive download qui va que jusqu'à 30 minutes

--- wp-content/themes/Radiotech/js/stream.js
+++ wp-content/themes/Radiotech/js/stream.js
@@ -1,18 +1,7 @@
 $(document).ready(function(){
 	$("#button").on('click', function(){
 		var filename = $(this).data("name");
-		$("#audio_player").attr("src", "/wordpress/wp-content/plugins/videostream/streaming.php?filename="+filename);
+		$("#audio_player").attr("src", "/wordpress/wp-content/plugins/audiostream/streaming.php?filename="+filename);
 		$('#audio_player').get(0).play();
-
-		/*jQuery.post(
-		    ajaxurl,
-		    {
-		        'action': 'read_stream',
-		        'param': filename
-		    },
-		    function(response){
-		    	console.log(response);
-		    }
-		);*/
 	});
 });
