844a0ac6daa28653ef0c65d1157775461531f879
aramassamy23 <ramassamy.ashok@gmail.com>
2016-05-25 15:06:28 +0200

    slider css

--- wp-content/themes/Radiotech/front-page.php
+++ wp-content/themes/Radiotech/front-page.php
@@ -9,12 +9,14 @@ get_header();
         <h2>Informatique</h2>
         <div>
             <section>
-                <span class="categorie_slide"> <img src="<?php radiotech_th(); ?>/logo-rond.PNG" alt="play"><span>Lire la radio </span><BR>informatique</span>
+                <span class="categorie_slide"> <div class="logo-rond"></div><span>Lire la radio </span><BR>informatique</span>
             </section>
         </div>
+        <div>
         <section>
-            <span class="chiffres">Déjà 10 456 abonnés<BR> et 111 530 téléchargements de podcasts !</span>
+            <span class="chiffres">Déjà <span>10 456</span> abonnés<BR> et <span>111 530</span> téléchargements de podcasts !</span>
         </section>
+          </div>   
     </div>
     <section>
         <div class="container">
